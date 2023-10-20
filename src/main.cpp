97af394f8879cbf811f4b93bbe2f1aaf97476330
Kira <kira.eva@protonmail.com>
2023-10-20 03:53:05 +0100

    draw rect, and add support for textures

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-﻿#include <Glad/glad.h>
+﻿#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "SHADER.h"
 #include <iostream>
@@ -52,29 +52,42 @@ int main() {
     // -------------------- SHADER COMPILATION END---------------------------
     // @formatter:off
     float vertices[] = {
-            // positions            //colors
-             0.5f,  -0.5f,  0.0f,   1.0f, 0.0f, 0.0f,   // bottom right
-            -0.5f,  -0.5f,  0.0f,   0.0f, 1.0f, 0.0f,   // bottom left
-             0.0f,   0.5f,  0.0f,   0.0f, 0.0f, 1.0f,   // top
+            // positions            // colors          // texture coords
+             0.5f,   0.5f,  0.0f,   1.0f, 0.0f, 0.0f,  1.0f, 1.0f, // top right
+             0.5f,  -0.5f,  0.0f,   0.0f, 1.0f, 0.0f,  1.0f, 1.0f, // bottom right
+            -0.5f,  -0.5f,  0.0f,   0.0f, 0.0f, 1.0f,  1.0f, 0.0f, // bottom left
+            -0.5f,   0.5f,  0.0f,   1.0f, 1.0f, 0.0f,  0.0f, 1.0f  // top left
     };
+
+    unsigned int indices[] = {
+            0, 1, 3, // first triangle
+            1, 2, 3  // second triangle
+    };
+    
     // @formatter:on
 
     unsigned int VBO;
     unsigned int VAO;
+    unsigned int EBO;
 
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) nullptr);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *) nullptr);
     glEnableVertexAttribArray(0);
 
     // color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) (3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *) (3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
 //    setWireframeMode(wireframeModeOn);
@@ -106,7 +119,7 @@ int main() {
         // ---------------
         ourShader.use();
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
         // swap buffers and handle I/O
         glfwSwapBuffers(window);
